a2a476e53e149aba0c156bd115db46f1d9fc5cd9
Kunal Mishra <kmishra9@berkeley.edu>
2018-07-30 15:17:07 -0700

    Create HW0_Skeleton.R

---
+++ HW0_Skeleton.R
@@ -0,0 +1,21 @@
+# Test OkR HW0 Skeleton File
+
+library(nycflights13)
+
+# Problem 1
+# Assign two integers of your choosing to the values x and y.
+
+x = ...
+y = ...
+
+# Problem 2
+# Assign the product of x and y to z
+
+z = ...
+
+# Problem 3
+# Take the remainder of z divided by 100 and assign it to a. Then, get the first a rows from 
+# the nycflights13 dataset and save them in the subsetFlights variable
+a = ...
+
+subsetFlights = ...
